34086279ec04b7841399095456362dd31d91ca5b
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2023-06-19 14:51:08 +0200

    create pokemonlist

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-let favoriteFood = "Pasta";
+let pokemonList = [
+    {
+    name: 'Balbasaur',
+    height: 7,
+    types: ['grass', 'poison'],
+},
+{
+    name: 'Charmander',
+    height: 0.6,
+    types: 'fire',
+},
+{
+    name: 'squirtle',
+    height: 0.5,
+    types: 'water',
+},
+];
 
-document.write(favoriteFood)
-
-alert("Hello world");
